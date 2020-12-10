826886518c5bd17e14ff3da247ac23fb2d92c214
mrraxa01 <mrraxa01@gmail.com>
2020-12-09 22:58:24 -0300

    Mostrando imagens das categorias

--- src/config/api.config.ts
+++ src/config/api.config.ts
@@ -1,10 +1,9 @@
 
 
 export const API_CONFIG = {
-    baseUrl: "http://localhost:8080"
+    baseUrl: "http://localhost:8080",
+    bucketBaseUrl: "https://projetosbi.s3-sa-east-1.amazonaws.com"
 }
 
-/*export const API_CONFIG = {
-    baseUrl: "http://localhost:8080"
-    
-}*/
+
+ 
--- src/pages/categorias/categorias.ts
+++ src/pages/categorias/categorias.ts
@@ -1,13 +1,8 @@
 import { Component } from '@angular/core';
 import { IonicPage, NavController, NavParams } from 'ionic-angular';
+import { API_CONFIG } from '../../config/api.config';
+import { CategoriaDTO } from '../../models/categoria.dto';
 import { CategoriaService } from '../../services/domain/categoria.service';
-
-/**
- * Generated class for the CategoriasPage page.
- *
- * See https://ionicframework.com/docs/components/#navigation for more info on
- * Ionic pages and navigation.
- */
 
 @IonicPage()
 @Component({
@@ -15,6 +10,10 @@ import { CategoriaService } from '../../services/domain/categoria.service';
   templateUrl: 'categorias.html',
 })
 export class CategoriasPage {
+
+  bucketUrl: string = API_CONFIG.bucketBaseUrl;
+
+  items: CategoriaDTO[];
 
   constructor(
     public navCtrl: NavController, 
@@ -26,7 +25,7 @@ export class CategoriasPage {
    
     this.categoriaService.findAll()
     .subscribe(response => {
-      console.log(response);
+      this.items = response;
     },
     error => {
       console.log(error);
