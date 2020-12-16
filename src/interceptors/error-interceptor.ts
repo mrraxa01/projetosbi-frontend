1203567a3a65e9ef1b5b9e1073e8e9cfa4bf1b83
mrraxa01 <mrraxa01@gmail.com>
2020-12-15 22:15:28 -0300

    Tratamento padrão para outros erros com Alert

--- src/interceptors/error-interceptor.ts
+++ src/interceptors/error-interceptor.ts
@@ -2,11 +2,12 @@ import { Injectable } from '@angular/core';
 import { HttpEvent, HttpInterceptor, HttpHandler, HttpRequest, HTTP_INTERCEPTORS } from '@angular/common/http';
 import { Observable } from 'rxjs/Rx'; // IMPORTANTE: IMPORT ATUALIZADO
 import { StorageService } from '../services/storage_service';
+import { AlertController } from 'ionic-angular';
 
 @Injectable()
 export class ErrorInterceptor implements HttpInterceptor {
 
-    constructor(public storage: StorageService){}
+    constructor(public storage: StorageService, public alertCrtl: AlertController){}
 
     intercept(req: HttpRequest<any>, next: HttpHandler): Observable<HttpEvent<any>> {
        // console.log("Passou no interceptor");
@@ -26,18 +27,48 @@ export class ErrorInterceptor implements HttpInterceptor {
 
             //permite tratar cada erro especificamente
             switch(errorObj.status){
+                case 401:
+                    this.handle401();
+                    break;
                 case 403:
                     this.handle403();
                     break;
+
+                default:
+                    this.handleDefaulError(errorObj);
             }
 
             return Observable.throw(errorObj);
         }) as any;
     }
 
+    handle401(){
+        let alert = this.alertCrtl.create({
+            title : 'Erro 401: Falha de Autenticação!!',
+            message: 'Email ou senha incorretos!!',
+            enableBackdropDismiss: false, // só sai do alert clicando em fechar
+            buttons:[{
+                    text : 'Ok'
+            }]
+
+        });
+        alert.present();
+    }    
     //caso ocorra o 403(usuário inválido) vai forçar o zeramento do storage
     handle403(){
         this.storage.setLocalUser(null);
+    }
+    handleDefaulError(errorObj){
+        let alert = this.alertCrtl.create({
+            title : 'Erro' +errorObj.status +': ' + errorObj.error,
+            message: errorObj.message,
+            enableBackdropDismiss: false, // só sai do alert clicando em fechar
+            buttons:[{
+                    text : 'Ok'
+            }]
+
+        });
+        alert.present();
     }
 }
 
