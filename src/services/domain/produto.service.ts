94f9018bfd82e67685f4583b5b8f23fd4d48da00
mrraxa01 <mrraxa01@gmail.com>
2021-01-20 18:52:59 -0300

    Infinite Scroll

--- src/services/domain/produto.service.ts
+++ src/services/domain/produto.service.ts
@@ -18,9 +18,10 @@ constructor(public http: HttpClient){
     return this.http.get<ProdutoDTO>(`${API_CONFIG.baseUrl}/produtos/${produto_id}`);
 
   }
-
-  findByCategoria(categoria_id:string){
-    return this.http.get(`${API_CONFIG.baseUrl}/produtos/?categorias=${categoria_id}`);
+//page e linerPerPage devem ter o msm nome do backend
+//page e linerPerPage se não for passado no método vai carregar com valores conforme abaixo 0 e 24
+  findByCategoria(categoria_id:string, page: number =0, linesPerPage: number = 24){
+    return this.http.get(`${API_CONFIG.baseUrl}/produtos/?categorias=${categoria_id}&page=${page}&linesPerPage=${linesPerPage}`);
   }
 
   getSmallImageFromBucket(id : string) : Observable<any>{
