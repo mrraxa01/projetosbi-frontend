fd06d817bd45681c7e0f40a48a4dd8d5a4e095ec
mrraxa01 <mrraxa01@gmail.com>
2021-01-19 23:25:44 -0300

    Informando pedido registrato ao usuário

--- src/pages/order-confirmation/order-confirmation.ts
+++ src/pages/order-confirmation/order-confirmation.ts
@@ -19,6 +19,7 @@ export class OrderConfirmationPage {
   cartItems: CartItem[];
   cliente: ClienteDTO;
   endereco: EnderecoDTO;
+  codpedido: string;
 
   constructor(
     public navCtrl: NavController,
@@ -55,11 +56,14 @@ private findEndereco(id: string, list: EnderecoDTO[]) : EnderecoDTO{
     back(){
       this.navCtrl.setRoot('CartPage');
     }
+    home(){
+      this.navCtrl.setRoot('CategoriasPage');
+    }
     checkout(){
       this.pedidoService.insert(this.pedido)
       .subscribe(response => {
         this.cartService.createOrClearCart();
-        console.log(response.headers.get('location'));
+        this.codpedido = this.extractId(response.headers.get('location'));
       },
         error => {
           if(error.status == 403){
@@ -67,6 +71,11 @@ private findEndereco(id: string, list: EnderecoDTO[]) : EnderecoDTO{
           }
 
         });
+    }
+    private extractId(location : string): string {
+      let position = location.lastIndexOf('/');
+      return location.substring(position +1, location.length);
+
     }
 
 }
