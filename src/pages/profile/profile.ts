9076c3cc9a8a5925c3797daabb02cb745cc89347
mrraxa01 <mrraxa01@gmail.com>
2021-01-24 20:10:26 -0300

    Bugfix - Atualização imagem de perfil

--- src/pages/profile/profile.ts
+++ src/pages/profile/profile.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { DomSanitizer } from '@angular/platform-browser';
 import { Camera, CameraOptions } from '@ionic-native/camera';
 import { IonicPage, NavController, NavParams } from 'ionic-angular';
 import { API_CONFIG } from '../../config/api.config';
@@ -17,13 +18,17 @@ export class ProfilePage {
 
   cliente : ClienteDTO;
   picture: string; //string pq a imagem sera base64
+  profileImage;
   cameraOn: boolean = false; //vai inibir o botão da câmera qdo tiver ativa
 
   constructor(public navCtrl: NavController,
      public navParams: NavParams,
      public storage : StorageService,
      public clienteService : ClienteService,
-     public camera: Camera) {
+     public camera: Camera,
+     public sanitizer: DomSanitizer,
+     ){
+      this.profileImage = 'assets/imgs/avatar-blank.png';
   }
 
   ionViewDidLoad() {
@@ -56,10 +61,24 @@ export class ProfilePage {
     this.clienteService.getImageFromBucket(this.cliente.id)
     .subscribe(response => {
       this.cliente.imageUrl = `${API_CONFIG.bucketBaseUrl}/cp${this.cliente.id}.jpg`;
+      this.blobToDataURL(response).then(dataURL =>{
+        let str: string = dataURL as string;
+        this.profileImage = this.sanitizer.bypassSecurityTrustUrl(str);
+      });
     },
-    error => {});
+    error => {
+      this.profileImage = 'assets/imgs/avatar-blank.png';
+    });
   }
 
+  blobToDataURL(blob) {
+    return new Promise((fulfill, reject) => {
+        let reader = new FileReader();
+        reader.onerror = reject;
+        reader.onload = (e) => fulfill(reader.result);
+        reader.readAsDataURL(blob);
+    });
+  }
   getCameraPicture(){
     this.cameraOn = true;
 
@@ -101,7 +120,7 @@ export class ProfilePage {
       this.clienteService.uploadPicture(this.picture)
       .subscribe(response => {
         this.cancel();
-        this.loadData();
+        this.getImageIfExists();
       },
       error => {});
     }
